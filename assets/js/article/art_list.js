1411c7aa8f62e3ff0a9994bb9230eb11128f3ff6
it-fan <it@46129.cn>
2023-02-21 13:56:04 +0800

    文章分支

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,15 +1,19 @@
 $(function () {
+  let layer = layui.layer
+  let form = layui.form
+  let laypage = layui.laypage
+
   // 定义美化时间的过滤器
   template.defaults.imports.dataFormat = function (date) {
     const dt = new Date(date)
 
-    const y = dt.getFullYear()
-    const m = padZero(dt.getMonth() + 1)
-    const d = padZero(dt.getDate())
+    var y = dt.getFullYear()
+    var m = padZero(dt.getMonth() + 1)
+    var d = padZero(dt.getDate())
 
-    const hh = padZero(dt.getHours())
-    const mm = padZero(dt.getMinutes())
-    const ss = padZero(dt.getSeconds())
+    var hh = padZero(dt.getHours())
+    var mm = padZero(dt.getMinutes())
+    var ss = padZero(dt.getSeconds())
 
     return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
   }
@@ -21,14 +25,16 @@ $(function () {
 
   // 定义一个查询的参数对象，将来请求数据的时候，
   // 需要将请求参数对象提交到服务器
-  const q = {
+  var q = {
     pagenum: 1, // 页码值，默认请求第一页的数据
     pagesize: 2, // 每页显示几条数据，默认每页显示2条
     cate_id: '', // 文章分类的 Id
     state: '' // 文章的发布状态
   }
+
   initTable()
   initCate()
+
   // 获取文章列表数据的方法
   function initTable() {
     $.ajax({
@@ -40,8 +46,10 @@ $(function () {
           return layer.msg('获取文章列表失败！')
         }
         // 使用模板引擎渲染页面的数据
-        let htmlStr = template('tpl-table', res)
+        var htmlStr = template('tpl-table', res)
         $('tbody').html(htmlStr)
+        // 调用渲染分页的方法
+        renderPage(res.total)
       }
     })
   }
@@ -56,7 +64,7 @@ $(function () {
           return layer.msg('获取分类数据失败！')
         }
         // 调用模板引擎渲染分类的可选项
-        var htmlStr = template('tpl-cate', res)
+        let htmlStr = template('tpl-cate', res)
         $('[name=cate_id]').html(htmlStr)
         // 通过 layui 重新渲染表单区域的UI结构
         form.render()
